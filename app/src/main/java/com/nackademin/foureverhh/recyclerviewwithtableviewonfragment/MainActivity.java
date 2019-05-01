8bfa776f1bafc2a737c6564d158c317f89e5ab0e
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-05-01 16:40:06 +0200

    Set up viewpager on tablayout

--- app/src/main/java/com/nackademin/foureverhh/recyclerviewwithtableviewonfragment/MainActivity.java
+++ app/src/main/java/com/nackademin/foureverhh/recyclerviewwithtableviewonfragment/MainActivity.java
@@ -1,13 +1,41 @@
 package com.nackademin.foureverhh.recyclerviewwithtableviewonfragment;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout_id);
+        viewPager = findViewById(R.id.viewPager_id);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        //Add Fragment Here
+        viewPagerAdapter.addFragment(new FragmentCall(),"Call");
+        viewPagerAdapter.addFragment(new FragmentContact(),"Contact");
+        viewPagerAdapter.addFragment(new FragmentFav(),"Favorite");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_call);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_group);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_star);
+
+        //Remove shadow from action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setElevation(0);
     }
 }
---
+++ app/src/main/java/com/nackademin/foureverhh/recyclerviewwithtableviewonfragment/ViewPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.nackademin.foureverhh.recyclerviewwithtableviewonfragment;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> listFragments = new ArrayList<>();
+    private final List<String> listTitles = new ArrayList<>();
+
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return listFragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return listFragments.size();
+    }
+
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return listTitles.get(position);
+    }
+
+    public void addFragment(Fragment fragment,String title){
+        listFragments.add(fragment);
+        listTitles.add(title);
+    }
+}
